4552c8f5970acc6295ea8e45504e2a24350d04b8
Ikhsan <ikhsan.budi.wicaksono@students.untidar.ac.id>
2024-08-26 21:04:54 +0700

    Update fetch_medium_posts.py

--- scripts/fetch_medium_posts.py
+++ scripts/fetch_medium_posts.py
@@ -4,6 +4,9 @@ import re
 # Medium feed URL
 MEDIUM_FEED_URL = "https://medium.com/feed/@ikhsan.budi.wicaksono"
 
+# Files
+README_FILE = "README.md"
+
 # Fetch Medium RSS feed
 feed = feedparser.parse(MEDIUM_FEED_URL)
 
@@ -13,26 +16,32 @@ def extract_image(content):
     return img.group(1) if img else None
 
 # Generate the README content
-with open("README.md", "r") as file:
-    readme = file.read()
+def generate_medium_posts():
+    posts = []
+    for entry in feed.entries[:5]:  # Change this number to limit posts
+        title = entry.title
+        url = entry.link
+        img_url = extract_image(entry.content[0].value) if 'content' in entry else None
+        if img_url:
+            post = f'<a href="{url}" target="_blank"><img src="{img_url}" alt="{title}" width="300"/></a>\n\n[{title}]({url})'
+        else:
+            post = f'[{title}]({url})'
+        posts.append(post)
+    return "\n\n".join(posts)
 
-start_tag = "<!-- BLOG-POST-LIST:START -->"
-end_tag = "<!-- BLOG-POST-LIST:END -->"
-before_start = readme.split(start_tag)[0]
-after_end = readme.split(end_tag)[1]
+def update_readme():
+    new_posts = generate_medium_posts()
 
-posts = []
-for entry in feed.entries[:5]:  # Change this number to limit posts
-    title = entry.title
-    url = entry.link
-    img_url = extract_image(entry.content[0].value) if 'content' in entry else None
-    if img_url:
-        post = f'<a href="{url}" target="_blank"><img src="{img_url}" alt="{title}" width="300"/></a>\n\n[{title}]({url})'
-    else:
-        post = f'[{title}]({url})'
-    posts.append(post)
+    with open(README_FILE, "r") as f:
+        readme_content = f.read()
 
-new_content = start_tag + "\n" + "\n\n".join(posts) + "\n" + end_tag
+    start_tag = "<!-- BLOG-POST-LIST:START -->"
+    end_tag = "<!-- BLOG-POST-LIST:END -->"
 
-with open("README.md", "w") as file:
-    file.write(before_start + new_content + after_end)
+    new_content = readme_content.split(start_tag)[0] + start_tag + "\n" + new_posts + "\n" + end_tag + readme_content.split(end_tag)[1]
+    
+    with open(README_FILE, "w") as f:
+        f.write(new_content)
+
+if __name__ == "__main__":
+    update_readme()
