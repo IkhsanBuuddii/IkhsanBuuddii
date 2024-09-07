25b6466533b5bbf0bdb4a073b664d1075bacd3ef
Ikhsan <ikhsan.budi.wicaksono@students.untidar.ac.id>
2024-09-07 19:00:51 +0700

    Update update_readme.py
    Menambahkan penghapusan konten lama

--- update_readme.py
+++ update_readme.py
@@ -1,3 +1,5 @@
+import re
+
 README_FILE = "README.md"
 POSTS_FILE = "scripts/posts.txt"
 
@@ -8,7 +10,13 @@ def update_readme():
     with open(README_FILE, "r") as f:
         readme_content = f.read()
 
-    new_content = readme_content.replace("<!-- START_MEDIUM_POSTS -->", "<!-- END_MEDIUM_POSTS -->\n" + posts)
+    # Gunakan regex untuk mengganti konten di antara START dan END
+    new_content = re.sub(
+        r"<!-- START_MEDIUM_POSTS -->(.*?)<!-- END_MEDIUM_POSTS -->", 
+        f"<!-- START_MEDIUM_POSTS -->\n{posts}\n<!-- END_MEDIUM_POSTS -->", 
+        readme_content, 
+        flags=re.DOTALL
+    )
     
     with open(README_FILE, "w") as f:
         f.write(new_content)
